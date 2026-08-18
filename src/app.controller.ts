d70854122ff483b9014931d2055882998a0538d7
AlexOneWLRD <116282471+AlexOneWLRD@users.noreply.github.com>
2026-08-18 22:59:16 +0300

    BWNJS: upd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,13 @@
-import {
-    Controller,
-    Get,
-    Param,
-} from '@nestjs/common';
-import {UserService} from "./user.sevice";
-import {User} from "../generated/prisma/client";
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.sevice';
+import { User } from '../generated/prisma/client';
 
-
-// добавляем префикс пути
 @Controller('api')
 export class AppController {
-    constructor(
-        private readonly userService: UserService,
-    ) {}
+  constructor(private readonly userService: UserService) {}
 
-    @Get('post/:id')
-    async getPostById(@Param('id') id: string): Promise<User[]> {
-        return this.userService.users();
-    }
+  @Get('users')
+  async getUsers(): Promise<User[]> {
+    return this.userService.users();
+  }
 }
